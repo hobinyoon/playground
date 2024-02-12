9e58f00135afe85452ca6d150ef9873b9647fcd8
Your Name <hobinyoon@gmail.com>
2024-02-12 15:51:05 -0800

    Memory leak detection with leaks on MacOS

---
+++ leaks-hello-world/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void A() {
+  int* i = new int;
+}
+
+int main(int argc, char** argv){
+  // Interesting that leaks didn't detect the leak here
+  int* i = new int;
+
+  // It detected the leak here
+  A();
+
+  system("leaks hello-world");
+  return 0;
+}
