6a71f5eda8dff074acfd442a460277493809d9fd
Hobin Yoon <hobinyoon@gmail.com>
2024-09-23 21:46:06 -0700

    Unpack vector in compile time

--- vector-unpack-to-absl-strformat/hello_world.cc
+++ vector-unpack-to-absl-strformat/hello_world.cc
@@ -1,66 +1,55 @@
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "absl/strings/str_format.h"
 
+namespace Tuple {
+
 template <typename... Args, std::size_t... I>
 void print_tuple_imp(const std::tuple<Args...>& tup,
                      std::index_sequence<I...>) {
-  std::cout << "[" << __LINE__ << "]\n";
   // print_all(std::get<I>(tup)...);
   std::cout << absl::StrFormat("%v-%v-%v", std::get<I>(tup)...) << "\n";
-  std::cout << "[" << __LINE__ << "]\n";
 }
 
 template <typename... Args>
 void print_tuple(const std::tuple<Args...>& tup) {
-  std::cout << "[" << __LINE__ << "]\n";
   print_tuple_imp(tup, std::index_sequence_for<Args...>());
 }
 
-void UnpackTuple() {
-  std::cout << "[" << __LINE__ << "]\n";
+void Unpack() {
   print_tuple(std::make_tuple(1, 3.14, std::string("abc")));
-  std::cout << "[" << __LINE__ << "]\n";
 }
 
-// template<typename T>
-// void print_vector(const std::vector<T>& v){
-//   std::cout << "[" << __LINE__ << "]\n";
-//   std::cout << std::index_sequence_for<T>() << "\n";
-//   // print_vector_imp(v, std::index_sequence_for<T>(v));
-// }
+};  // namespace Tuple
 
-template <std::size_t... ParamsIdx>
-auto funcPointer(std::index_sequence<ParamsIdx...>) {
-  std::cout << "[" << __LINE__ << "]\n";
-  // std::cout << absl::StrFormat("%v-%v", std::get<I>()...) << "\n";
-  return 0;
-  // return [](std::vector<std::string> const &args) {
-  //     assert(args.size() == sizeof...(ParamsIdx));
-  //     return f(args[ParamsIdx]...);
-  // };
+namespace Vector {
+
+template <size_t... I>
+void PrintVec_(const std::vector<int>& args, const std::index_sequence<I...>) {
+  std::cout << absl::StrFormat("%v-%v", args[I]...) << "\n";
 }
 
-template <typename... Params>
-auto funcPointer(Params...) {
-  std::cout << "[" << __LINE__ << "]\n";
-  return funcPointer(std::index_sequence_for<Params...>{});
+template <size_t num_args>
+void PrintVec(const std::vector<int>&& args) {
+  assert(args.size() == num_args);  // just to be sure
+  PrintVec_(args, std::make_index_sequence<num_args>{});
 }
 
-void UnpackVector() {
-  std::cout << "[" << __LINE__ << "]\n";
-  funcPointer(std::vector<int>{1, 2});
-  std::cout << "[" << __LINE__ << "]\n";
+void Unpack() {
+  // This has to be a compile time argument
+  PrintVec<2>(std::vector<int>{1, 2});
 }
+}
+;  // namespace Vector
 
 int main() {
-  UnpackTuple();
+  Tuple::Unpack();
   std::cout << "\n";
 
-  // TODO: Getting close ...
-  UnpackVector();
+  Vector::Unpack();
 
   return 0;
 }
