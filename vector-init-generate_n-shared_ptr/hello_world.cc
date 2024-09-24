bafa4ad22ca17bbb164b2db0ea3666870a377051
Hobin Yoon <hobinyoon@gmail.com>
2024-09-24 13:34:39 -0700

    Add more init examples

--- vector-init-generate_n-shared_ptr/hello_world.cc
+++ vector-init-generate_n-shared_ptr/hello_world.cc
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <vector>
 
+#define LOG std::cout << "[" << __LINE__ << "] "
+
 void VectorInitWithUniquePtr() {
   std::vector<std::unique_ptr<int>> vec;
   std::generate_n(std::back_inserter(vec), 2,
                   []() { return std::make_unique<int>(2); });
   for (const auto& v : vec) {
-    std::cout << &v << " " << v << " " << v.get() << " " << *v << "\n";
+    LOG << &v << " " << v << " " << v.get() << " " << *v << "\n";
   }
 }
 
 void VectorInitWithSharedPtr() {
   std::vector<std::shared_ptr<int>> vec;
   std::generate_n(std::back_inserter(vec), 2, [n = 0]() mutable {
-    std::cout << "[" << __LINE__ << "]\n";
+    LOG << "\n";
     return std::make_shared<int>(++n);
   });
-  std::cout << "[" << __LINE__ << "]\n";
   for (const auto& v : vec) {
-    std::cout << "[" << __LINE__ << "] " << &v << " " << v << " " << v.get()
-              << " " << *v << "\n";
+    LOG << &v << " " << v << " " << v.get() << " " << *v << "\n";
   }
 }
 
@@ -27,18 +27,35 @@ void VectorInitWithSharedPtr2() {
   std::vector<std::shared_ptr<int>> vec;
   for (int i = 0; i < 2; ++i) vec.push_back(std::make_shared<int>(2));
   for (const auto& v : vec) {
-    std::cout << &v << " " << v << " " << v.get() << " " << *v << "\n";
+    LOG << &v << " " << v << " " << v.get() << " " << *v << "\n";
+  }
+}
+
+// Seems like you can't initialize arr. It is nullptr by default.
+void VectorInitWithSharedPtr3() {
+  std::array<std::shared_ptr<int>, 2> arr;
+  for (const auto& v : arr) {
+    LOG << &v << " " << v << " " << v.get() << "\n";
+  }
+}
+
+// You end up having 2 shared_ptrs pointing the same object
+void VectorInitWithSharedPtr4() {
+  static int a = 100;
+  std::vector<std::shared_ptr<int>> vec(2, std::make_shared<int>(++a));
+  for (const auto& v : vec) {
+    LOG << &v << " " << v << " " << v.get() << " " << *v << "\n";
   }
 }
 
 void ArrayInitWithSharedPtr() {
   std::array<std::shared_ptr<int>, 2> arr;
   std::generate_n(arr.begin(), 2, [n = 0]() mutable {
-    std::cout << "[" << __LINE__ << "]\n";
+    LOG << "\n";
     return std::make_shared<int>(++n);
   });
   for (const auto& a : arr) {
-    std::cout << &a << " " << a << " " << a.get() << " " << *a << "\n";
+    LOG << &a << " " << a << " " << a.get() << " " << *a << "\n";
   }
 }
 
@@ -46,6 +63,8 @@ int main() {
   VectorInitWithUniquePtr();
   VectorInitWithSharedPtr();
   VectorInitWithSharedPtr2();
+  VectorInitWithSharedPtr3();
+  VectorInitWithSharedPtr4();
   ArrayInitWithSharedPtr();
   return 0;
 }
