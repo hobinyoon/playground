f3e0d5805bb54171abdd06231dfcdd829ddcf2eb
Hobin Yoon <hobinyoon@gmail.com>
2025-01-10 05:37:19 +0000

    Clean up

--- memory-leak-detection/asan/hello_world.cc
+++ memory-leak-detection/asan/hello_world.cc
@@ -4,8 +4,6 @@
 
 #define LOG std::cout << "[" << __LINE__ << "] "
 
-// TODO: Move the leaks directory to here
-
 int main() {
   // Asan detected this leak on Ubuntu18, but not on MacOS 15.2.
   int* i = new int(1);
